3bd82dddf630b28bf1e41b7402b64dec863706e7
dmar <dan.britain.marshall@gmail.com>
2019-03-22 22:31:13 -0500

    working commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('New Bacon-ings Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Foot Feta-ish Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Mission A-Corn-Plished Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Child Molester Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hit Me With Your Best Shallot Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
